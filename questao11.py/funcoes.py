8e24de1975cb53f38c1fb4e0cb8af3a9dcc1ff19
HenriqueMartins de Melo <rickmmelo7h@gmail.com>
2026-04-30 10:35:28 -0300

    questao11corrigida

--- questao11.py/funcoes.py
+++ questao11.py/funcoes.py
@@ -1,10 +1,11 @@
-from funcoes import calcula_pontos_quina, calcula_pontos_full_house, calcula_pontos_quadra, calcula_pontos_sem_combinacao, calcula_pontos_sequencia_alta, calcula_pontos_sequencia_baixa
+from funcoes import calcula_pontos_quina, calcula_pontos_full_house, calcula_pontos_quadra, calcula_pontos_soma, calcula_pontos_sequencia_alta, calcula_pontos_sequencia_baixa
+
 def calcula_pontos_regra_avancada(dados):
     return {
         'cinco_iguais': calcula_pontos_quina(dados),
         'full_house': calcula_pontos_full_house(dados),
         'quadra': calcula_pontos_quadra(dados),
-        'sem_combinacao': calcula_pontos_sem_combinacao(dados),
+        'sem_combinacao': calcula_pontos_soma(dados),
         'sequencia_alta': calcula_pontos_sequencia_alta(dados),
         'sequencia_baixa': calcula_pontos_sequencia_baixa(dados)
     }
